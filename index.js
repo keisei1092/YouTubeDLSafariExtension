21bea187e1c0155bfb1aee15249666265760163b
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-22 17:08:22 +0900

    use pow addresses

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var target = $('.yt-lockup-meta-info');
 // top
 $.each(target, function() {
   var videoId = $(this).closest('.yt-lockup').data('context-item-id');
-  var element = '<li><a href="http://localhost:4568/youtube-dl/' + videoId + '" target="_blank">mp3</a>';
+  var element = '<li><a href="http://server.dev/youtube-dl/' + videoId + '" target="_blank">mp3</a>';
   $(this).append(element);
 });
 
 // show
 if ($('#watch8-secondary-actions') != undefined) {
   var videoId = $('.yt-uix-videoactionmenu').data('video-id');
-  $('#watch8-secondary-actions').append('<div class="yt-uix-menu"><a href="http://localhost:4568/youtube-dl/' + videoId + '" target="_blank">mp3</a></div>');
+  $('#watch8-secondary-actions').append('<div class="yt-uix-menu"><a href="http://server.dev/youtube-dl/' + videoId + '" target="_blank">mp3</a></div>');
 }
 
 // show=>related
 $.each($('.stat.view-count'), function() {
   var videoId = $(this).closest('a').attr('href').substring(9, 100);
-  $(this).closest('a').append('<span class="stat"><a href="http://localhost:4568/youtube-dl/' + videoId + '" target="_blank">mp3</a></span>');
+  $(this).closest('a').append('<span class="stat"><a href="http://server.dev/youtube-dl/' + videoId + '" target="_blank">mp3</a></span>');
 });
