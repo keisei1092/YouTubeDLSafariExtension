016f0db736de7bd0b9e4ebf88659d12a56897cf6
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-22 16:34:12 +0900

    add download button to almost every areas

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var target = $('.yt-lockup-meta-info');
 
+// top
 $.each(target, function() {
   var videoId = $(this).closest('.yt-lockup').data('context-item-id');
   var element = '<li><a href="http://localhost:4568/youtube-dl/' + videoId + '" target="_blank">mp3</a>';
   $(this).append(element);
+});
+
+// show
+if ($('#watch8-secondary-actions') != undefined) {
+  var videoId = $('.yt-uix-videoactionmenu').data('video-id');
+  $('#watch8-secondary-actions').append('<div class="yt-uix-menu"><a href="http://localhost:4568/youtube-dl/' + videoId + '" target="_blank">mp3</a></div>');
+}
+
+// show=>related
+$.each($('.stat.view-count'), function() {
+  var videoId = $(this).closest('a').attr('href').substring(9, 100);
+  $(this).closest('a').append('<span class="stat"><a href="http://localhost:4568/youtube-dl/' + videoId + '" target="_blank">mp3</a></span>');
 });
